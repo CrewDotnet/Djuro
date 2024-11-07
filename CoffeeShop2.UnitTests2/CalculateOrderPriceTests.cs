e41acf626dccfa1154a5fa906654db4452164305
Djuro Kurbatfinski <djurapez@gmail.com>
2024-11-07 18:35:58 +0100

    add calculate coffee price unit tests

--- CoffeeShop2.UnitTests2/CalculateOrderPriceTests.cs
+++ CoffeeShop2.UnitTests2/CalculateOrderPriceTests.cs
@@ -10,12 +10,30 @@ namespace CoffeeShop2.UnitTests2
     public class CalculateOrderPriceTests
     {
         [Test]
-        public void Calculate_WhenCalled_ReturnsCalculatedPrice()
+        public void Calculate_WhenCalled_ReturnsLargeCoffeePrice()
         {
-            var coffee = new Coffee();
+            var coffee = new Coffee
+            {
+                BasePrice = 2,
+                Size = CoffeeSize.Large
+            };
+
             var expectedPrice = CalculateOrderPrice.Calculate(coffee);
 
-            Assert.That(calculatedPrice.Equals(expectedPrice));
+            Assert.That(expectedPrice, Is.EqualTo(coffee.BasePrice * 1.2m));
+        }
+        [Test]
+        public void Calculate_WhenCalled_ReturnsRegularCoffeePrice()
+        {
+            var coffee = new Coffee
+            {
+                BasePrice = 2,
+                Size = CoffeeSize.Regular
+            };
+
+            var expectedPrice = CalculateOrderPrice.Calculate(coffee);
+
+            Assert.That(expectedPrice, Is.EqualTo(coffee.BasePrice));
         }
 
     }
